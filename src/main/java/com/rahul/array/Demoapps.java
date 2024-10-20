fbb819e2cf56a13be726121353129a06e747fd1c
itsrahulchaudhary <rahulcy49@gmail.com>
2024-10-20 11:29:14 +0530

    loop added

---
+++ src/main/java/com/rahul/array/Demoapps.java
@@ -0,0 +1,10 @@
+package com.rahul.array;
+
+public class Demoapps {
+    public static void main(String[] args) {
+        System.out.println("I am in feature/test");
+        for(int i=0; i<10; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
